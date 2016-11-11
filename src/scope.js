056e57b88c083b82b9e60324604e4a9523b5222b
yinchangqing <165580405@qq.com>
2016-11-11 18:54:05 +0800

    angular value deep watch..

--- src/scope.js
+++ src/scope.js
@@ -1,17 +1,21 @@
 function Scope(){
 	this.$$watchers= [];
+	this.$$lastDirtyWatch = null;
 }
 function initWatchVal(){}
-Scope.prototype.$watch = function(watchFn, listenerFn){
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq){
 	var watcher= {
 		watchFn: watchFn,
 		listenerFn: listenerFn || function(){},
-		last:initWatchVal
+		last: initWatchVal,
+		valueEq: !!valueEq
 	};
 	this.$$watchers.push(watcher);
+	this.$$lastDirtyWatch = null;
 };
 Scope.prototype.$digest = function() {
 	var dirty,ttl=10;
+	this.$$lastDirtyWatch = null;
 	do{
 		dirty = this.$digestOnce();
 		if (dirty && !(ttl--)) {
@@ -25,12 +29,33 @@ Scope.prototype.$digestOnce = function() {
 	 _.forEach(this.$$watchers, function(watcher){
 	 	newValue = watcher.watchFn(self);
 	 	oldValue = watcher.last;
-	 	debugger;
-	 	if(newValue != oldValue) {
+	 	if(!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+	 		self.$$lastDirtyWatch = watcher;
 	 		watcher.listenerFn(newValue, oldValue == initWatchVal?newValue:oldValue, self);
-	 		watcher.last = newValue;
+	 		watcher.last = watcher.valueEq?_.cloneDeep(newValue):newValue;
 	 		dirty = true;
+	 	} else if (self.$$lastDirtyWatch == watcher) {
+	 		return false;
 	 	}
 	 });
 	 return dirty;
+};
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq){
+	 if (valueEq){
+	 	return _.isEqual(newValue, oldValue);
+	 } else {
+	 	return (newValue == oldValue || 
+	 		(typeof(newValue) == 'number' && typeof(oldValue) == 'number' && isNaN(newValue) && isNaN(oldValue)));
+	 }
+};
+
+Scope.prototype.$eval = function(expr, locals){
+	 return expr(this, locals); 
+};
+Scope.prototype.$apply = function(expr){
+	 try {
+	  	return this.$eval(expr);
+	  } finally {
+	  	this.$digest();
+	  } 
 };
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -84,14 +84,14 @@ describe('Scope', function() {
 				return scope.nameUpper;
 			}, function(newValue, oldValue, scope) {
 				if (newValue) {
-					scope.initial = newValue.substring(0, 1) + '.';	
+					scope.initial = newValue.substring(0, 1) + '.';
 				}
 			});
 			scope.$watch(function(scope) {
 				return scope.name;
 			}, function(newValue, oldValue, scope) {
 				if (newValue) {
-					scope.nameUpper = newValue.toUpperCase();	
+					scope.nameUpper = newValue.toUpperCase();
 				}
 			});
 
@@ -102,21 +102,74 @@ describe('Scope', function() {
 			scope.$digest();
 			expect(scope.initial).toBe("B.");
 		});
-		it('gives up on the watches after 10 iterations', function(){
+		it('gives up on the watches after 10 iterations', function() {
 			scope.counterA = 0;
 			scope.counterB = 0;
-			scope.$watch(function(scope){
+			scope.$watch(function(scope) {
 				return scope.counterA;
-			},function (newValue, oldValue, scope) {
-				 scope.counterB++; 
+			}, function(newValue, oldValue, scope) {
+				scope.counterB++;
 			});
-			scope.$watch(function(scope){
+			scope.$watch(function(scope) {
 				return scope.counterB;
-			},function (newValue, oldValue, scope) {
-				 scope.counterA++; 
+			}, function(newValue, oldValue, scope) {
+				scope.counterA++;
 			});
-			
-			expect(function(){scope.$digest();}).toThrow();
+
+			expect(function() {
+				scope.$digest();
+			}).toThrow();
+		});
+		it('ends the digest when the last watch is clean', function() {
+			scope.array = _.range(100);
+			var watchExecutions = 0;
+			_.times(100, function(i) {
+				scope.$watch(function(scope) {
+					watchExecutions++;
+					return scope.array[i];
+				}, function(newValue, oldValue, scope) {
+
+				});
+			});
+
+			scope.$digest();
+			expect(watchExecutions).toBe(200);
+
+			scope.array[0] = 401;
+			scope.$digest();
+			expect(watchExecutions).toBe(301);
+		});
+		it('does not end digest so new watchers are not run', function() {
+			scope.aValue = 'abc';
+			scope.counter = 0;
+
+			scope.$watch(function(scope) {
+				return scope.aValue;
+			},function (newValue, oldValue, scope) {
+				 scope.$watch(function (scope) {
+				 	 return scope.aValue; 
+				 },function (newValue, oldValue, scope) {
+				 	 scope.counter++; 
+				 }); 
+			});
+			scope.$digest();
+			expect(scope.counter).toBe(1);
+		});
+		it("compare based on value is enabled", function () {
+			 scope.aValue = [1,2,3];
+			 scope.counter = 0;
+			 scope.$watch(function(scope){
+			 	return scope.aValue;
+			 }, function (newValue, oldValue, scope) {
+			 	 scope.counter++; 
+			 }, true);
+
+			 scope.$digest();
+			 expect(scope.counter).toBe(1);
+
+			 scope.aValue.push(4);
+			 scope.$digest();
+			 expect(scope.counter).toBe(2);
 		});
 	});
 });
