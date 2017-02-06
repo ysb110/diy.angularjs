ff991ee70249266c6d72eb10d9feaf2784577eea
yinchangqing <165580405@qq.com>
2017-02-06 18:56:58 +0800

    local changes

--- src/parse.js
+++ src/parse.js
@@ -1,6 +1,16 @@
 /* jshint globalstrict: true */
 'use strict';
 
+var ESCAPES = {
+	'n': '\n',
+	'f': '\f',
+	'r': '\r',
+	't': '\t',
+	'v': '\v',
+	'\'': '\'',
+	'"': '"'
+};
+
 function Lexer() {
 
 }
@@ -13,6 +23,17 @@ Lexer.prototype.lex = function(text) {
 		this.ch = this.text.charAt(this.index);
 		if (this.isNumber(this.ch) || (this.ch === '.' && this.isNumber(this.peek()))) {
 			this.readNumber();
+		} else if (this.ch === '"' || this.ch === "'") {
+			this.readString(this.ch);
+		} else if (this.ch === '[' || this.ch === ']') {
+			this.tokens.push({
+				text: this.ch
+			});
+			this.index++;
+		} else if (this.isIdent(this.ch)) {
+			this.readIdent();
+		} else if (this.isWhitespace(this.ch)) {
+			this.index++;
 		} else {
 			throw 'Unexpected next character: ' + this.ch;
 		}
@@ -20,14 +41,84 @@ Lexer.prototype.lex = function(text) {
 
 	return this.tokens;
 }
+Lexer.prototype.readIdent = function() {
+	var text = '';
+	while (this.index < this.text.length) {
+		var ch = this.text.charAt(this.index);
+		if (this.isIdent(ch) || this.isNumber(ch)) {
+			text += ch;
+		} else {
+			break;
+		}
+		this.index++;
+	}
+	var token = {
+		text: text
+	};
+	this.tokens.push(token);
+};
+Lexer.prototype.readString = function(quote) {
+	this.index++;
+	var string = '';
+	var escape = false;
+	while (this.index < this.text.length) {
+		var ch = this.text.charAt(this.index);
+		if (escape) {
+			if (ch === 'u') {
+				var hex = this.text.substring(this.index + 1, this.index + 5);
+				if (!hex.match(/[\da-f]{4}/i)) {
+					throw 'Invalid unicode escape';
+				}
+				this.index += 4;
+				string += String.fromCharCode(parseInt(hex, 16));
+			} else {
+				var replacement = ESCAPES[ch];
+				if (replacement) {
+					string += replacement;
+				} else {
+					string += ch;
+				}
+			}
+			escape = false;
+		} else if (ch === quote) {
+			this.index++;
+			this.tokens.push({
+				text: string,
+				value: string
+			});
+		} else if (ch === '\\') {
+			escape = true;
+		} else {
+			string += ch;
+		}
+		this.index++;
+	}
+	throw 'Unmatched quote';
+};
+Lexer.prototype.isExpOperator = function(ch) {
+	return ch === '-' || ch === '+' || this.isNumber(ch);
+};
+Lexer.prototype.isIdent = function(ch) {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch === '_' || ch === '$';
+};
 Lexer.prototype.readNumber = function() {
 	var number = '';
 	while (this.index < this.text.length) {
 		var ch = this.text.charAt(this.index).toLowerCase();
-		if (ch === '.' || ch === 'e' || ch === '-' || ch === '+' || this.isNumber(ch)) {
+		if (ch === '.' || this.isNumber(ch)) {
 			number += ch;
 		} else {
-			break;
+			var nextCh = this.peek();
+			var prevCh = number.charAt(number.length - 1);
+			if (ch === 'e' && this.isExpOperator(nextCh)) {
+				number += ch;
+			} else if (this.isExpOperator(ch) && prevCh === 'e' && nextCh && this.isNumber(nextCh)) {
+				number += ch;
+			} else if (this.isExpOperator(ch) && prevCh === 'e' && (!nextCh || !this.isNumber(nextCh))) {
+				throw 'Invalid exponent';
+			} else {
+				break;
+			}
 		}
 		this.index++;
 	}
@@ -39,8 +130,12 @@ Lexer.prototype.readNumber = function() {
 Lexer.prototype.isNumber = function(ch) {
 	return '0' <= ch && ch <= '9';
 };
-Lexer.prototype.peek = function(){
-	return this.index < this.text.length - 1 ? this.text.charAt(this.index + 1) : false;  
+Lexer.prototype.peek = function() {
+	return this.index < this.text.length - 1 ? this.text.charAt(this.index + 1) : false;
+};
+Lexer.prototype.isWhitespace = function(ch) {
+	return ch === ' ' || ch === '\r' || ch === '\t' ||
+		ch === '\n' || ch === '\v' || ch === '\u00A0';
 };
 
 function AST(lexer) {
@@ -48,9 +143,24 @@ function AST(lexer) {
 }
 AST.Program = 'Program';
 AST.Literal = 'Literal';
+AST.ArrayExpression = 'ArrayExpression';
 AST.prototype.ast = function(text) {
 	this.tokens = this.lexer.lex(text);
 	return this.program();
+};
+AST.prototype.constants = {
+	'null': {
+		type: AST.Literal,
+		value: null
+	},
+	'true': {
+		type: AST.Literal,
+		value: true
+	},
+	'false': {
+		type: AST.Literal,
+		value: false
+	}
 };
 AST.prototype.program = function() {
 	return {
@@ -63,6 +173,36 @@ AST.prototype.constant = function() {
 		type: AST.Literal,
 		value: this.tokens[0].value
 	};
+};
+
+AST.prototype.primary = function() {
+	if (this.expect('[')) {
+		return this.arrayDeclaration();
+	} else if (this.constants.hasOwnProperty(this.tokens[0].text)) {
+		return this.constants[this.tokens[0].text];
+	} else {
+		return this.constant();
+	}
+};
+AST.prototype.expect = function(e) {
+	if (this.tokens.length > 0) {
+		if (this.tokens[0].text === e || !e) {
+			return this.tokens.shift();
+		}
+	}
+};
+AST.prototype.arrayDeclaration = function() {
+	this.consume(']');
+	return {
+		type: AST.ArrayExpression
+	};
+};
+AST.prototype.consume = function(e) {
+	var token = this.expect(e);
+	if (!token) {
+		throw 'Unexpected. Expecting: ' + e;
+	}
+	return token;
 };
 
 function ASTCompiler(astBuilder) {
@@ -84,7 +224,22 @@ ASTCompiler.prototype.recurse = function(ast) {
 			this.state.body.push('return ', this.recurse(ast.body), ';');
 			break;
 		case AST.Literal:
-			return ast.value;
+			return this.escape(ast.value);
+		case AST.ArrayExpression:
+			return '[]';
+	}
+};
+ASTCompiler.prototype.stringEscapeRegex = /[^ a-zA-Z0-9]/g;
+ASTCompiler.prototype.stringEscapeFn = function(ch) {
+	return "\\u" + ('0000' + ch.charCodeAt(0).toString(16)).slice(-4);
+}
+ASTCompiler.prototype.escape = function(value) {
+	if (_.isString(value)) {
+		return "'" + value.replace(this.stringEscapeRegex, this.stringEscapeFn) + "'";
+	} else if (_.isNull(value)) {
+		return 'null';
+	} else {
+		return value;
 	}
 };
 
